545f79e55a64e40e559af3c6f8b0b66da3101331
dihuanjun <dihuanjun@fiture.com>
2021-06-11 14:10:09 +0800

    gitLearn 20210611

--- src/main/java/gitLearnPackage/gitLearnFile.java
+++ src/main/java/gitLearnPackage/gitLearnFile.java
@@ -5,8 +5,17 @@ public class gitLearnFile {
         System.out.println("hello git");
         System.out.println("hello git 第二个版本");
         System.out.println("hello git 第三个版本");
+        System.out.println("hot fix 修改");
     }
 }
+
+
+
+
+
+
+
+
 
 
 
